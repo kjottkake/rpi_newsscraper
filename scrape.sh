53560d40ebe56352a2d78adc015b19c7d207b7e3
Jason <jimanproductions@gmail.com>
2021-11-05 10:49:32 +0100

    updated script

--- scrape.sh
+++ scrape.sh
@@ -3,3 +3,11 @@
 #curl tv2 news
 curl  https://www.tv2.no/nyheter  > tv2nyheter.html
 
+
+opentag='<div class=\"article__content\">'
+closetag='<\/div>'
+#opentag='<article class=*>'
+#closetag='</article>'
+sed -n "/$opentag/,/$closetag/p" tv2nyheter.html > articles.html
+
+
