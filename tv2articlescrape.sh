17b0cbe8c02fb0527e748728d9d5849c9c9d0cb3
Jason <jimanproductions@gmail.com>
2021-12-04 18:17:22 +0100

    added counting number of articles

--- tv2articlescrape.sh
+++ tv2articlescrape.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+
+
 #check to see if initial file exists, if not exit
-FILE=./urls_tv2.txts
-if test -f "$FILE"; then
-	echo "$FILE exists."
+FILE=./urls_tv2.txt
+
+if test -f "$FILE"; then  		#checks to see if file exists
+	echo "$FILE exists."		#if the file exists print OK
+	ARTICLECOUNT=$(wc -l < "$FILE")	#saves number of articles into variable
+	echo $ARTICLECOUNT		#prints out how many articles there are 
 else
-	echo "ERROR: cannot find $FILE"
-fi
+	echo "ERROR: cannot find $FILE" #file not found
+fi  #end if
