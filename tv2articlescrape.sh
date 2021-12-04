bb5dce7a099e90f4c3e8c70b00d97e84555a8d0b
Jason <jimanproductions@gmail.com>
2021-12-04 18:34:27 +0100

    added pseudocode for looping section

--- tv2articlescrape.sh
+++ tv2articlescrape.sh
@@ -8,7 +8,11 @@ FILE=./urls_tv2.txt
 if test -f "$FILE"; then  		#checks to see if file exists
 	echo "$FILE exists."		#if the file exists print OK
 	ARTICLECOUNT=$(wc -l < "$FILE")	#saves number of articles into variable
-	echo $ARTICLECOUNT		#prints out how many articles there are 
+	echo $ARTICLECOUNT		#prints out how many articles there are
+	#here is where the logic starts
+	#for each article
+	#get the title 
+	#get the image 
 else
 	echo "ERROR: cannot find $FILE" #file not found
 fi  #end if
