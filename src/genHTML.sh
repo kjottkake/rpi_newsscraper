2c5823ec3c51839b2581a5cb4fd9154fb18c8602
Jason <jimanproductions@gmail.com>
2021-12-05 14:14:25 +0100

    fixed invalid use of line address 0 error

--- src/genHTML.sh
+++ src/genHTML.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
 #loop through files
-for ((i=0; i <=3; i++))
+for ((i=0; i<4; i++))
 do
+	echo $i
 	ARTICLE=./test/$((i+1)).txt
-	echo $ARTICLE
+	#echo $ARTICLE
+	for ((j=1; j<5; j++))
+	do
+		#echo $ARTICLE
+		LINE=$(sed "${j}q;d" $ARTICLE)
+		echo $LINE
+	done
 done
