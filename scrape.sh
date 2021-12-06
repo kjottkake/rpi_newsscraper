3c4576687cb6df34474d9aa46d3c4aa35064ae76
Jason <jimanproductions@gmail.com>
2021-12-06 15:32:00 +0100

    added comments to script

--- scrape.sh
+++ scrape.sh
@@ -2,21 +2,27 @@
 
 #get tv2 news and write into tv2nyheter file
 curl  https://www.tv2.no/nyheter  > tv2nyheter.html
+echo "Curled tv2.no/nyheter"
 
 #get articles elements from tv2nyheter file and write into tv2articles
-opentag='<article class=\"article column large-6 small-6 article--nyheter\">'
-closetag='<\/article>'
-sed -n "/$opentag/,/$closetag/p" tv2nyheter.html > tv2articles.html
+opentag='<article class=\"article column large-6 small-6 article--nyheter\">' #defining open tag search
+closetag='<\/article>'							      #defining close tag to search
+sed -n "/$opentag/,/$closetag/p" tv2nyheter.html > tv2articles.html          #-n argument to not print anything
+echo "Collected tv2 news articles"
 
 #get urls and make into new list
 cat tv2articles.html | grep 'href' > tv2articleUrls.html
+echo "Collected tv2 Article Urls"
 
-
+#current date method
 CURRENTDATE=$(date +"%Y-%m-%d")
 #create new directory
 mkdir ./src/articles/$CURRENTDATE
+echo "Created new directory ./src/articles/" + $CURRENTDATE
+
 #gets urls from article urls
 grep -oP '/nyheter/\K.*' tv2articleUrls.html | sed 's/\/\">//g' > urls_tv2.txt  
+echo "Created list of urls"
 
 #check to see if initial file exists, if not exit
 FILE=./urls_tv2.txt
