07de8b4f991f1d97f3306db9bf0e15832bbe4bea
Jason <jimanproductions@gmail.com>
2021-12-06 15:02:15 +0100

    get titles functional

--- src/articles/2021-12-04/getTitles.sh
+++ src/articles/2021-12-04/getTitles.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-opentag='<h1 itemprop=\"headline\" class=\"articleheader__title\">'
+#opentag='<h1 itemprop=\"headline\" class=\"articleheader__title\">'
+opentag='<h1'
 closetag='<\/h1>'
 
 grep $opentag test.html | sed -e 's/<[^>]*>//g' > title1.txt
