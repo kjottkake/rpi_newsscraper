ccc7d606aa9614095f968a6c19afc49f877333d9
Jason <jimanproductions@gmail.com>
2021-12-07 11:44:19 +0100

    working on generating html page

--- src/genHTML.sh
+++ src/genHTML.sh
@@ -6,10 +6,22 @@ do
 	echo $i
 	ARTICLE=./test/$((i+1)).txt
 	#echo $ARTICLE
-	for ((j=1; j<5; j++))
-	do
+	#for ((j=1; j<5; j++))
+	#do
 		#echo $ARTICLE
-		LINE=$(sed "${j}q;d" $ARTICLE)
-		echo $LINE
-	done
+		#LINE=$(sed "${j}q;d" $ARTICLE)
+		#echo $LINE
+	#done
+	URL=$(sed "1q;d" $ARTICLE)
+	TITLE=$(sed "2q;d" $ARTICLE)
+	IMG=$(sed "3q;d" $ARTICLE)
+	DATE=$(sed "4q;d" $ARTICLE)
+	echo $URL
+	echo $TITLE
+	echo $IMG
+
+	touch article$1.html
+	echo "
+	
+        "
 done
