446de8f429488cd7d78d38c9130b13f577e18e9a
Jason <jimanproductions@gmail.com>
2021-12-09 11:45:03 +0100

    updated css linking

--- scrape.sh
+++ scrape.sh
@@ -76,7 +76,7 @@ do
     		<meta http-equiv="X-UA-Compatible" content="IE=edge">
     		<meta name="viewport" content="width=device-width, initial-scale=1.0">
     		<title>$TITLE</title>
-    		<link rel="stylesheet" href="../style.css">
+    		<link rel="stylesheet" href="../../style.css">
 	</head>
 	<body>
     		<h1>$TITLE</h1>
